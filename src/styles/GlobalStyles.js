4fd06cf034dbe1f6f87a596a16a8e91f517a95a5
Baltazar <balti.kpo@gmail.com>
2022-04-06 19:09:50 -0300

    Fixed heigth bug

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,18 +3,16 @@ import styled from "styled-components";
 export const Div = styled.div`
   position: relative;
   overflow: hidden;
-  bottom: 0;
-  top: 0;
-  left: 0;
-  right: 0;
   color: #fafafa;
   display: flex;
   flex-direction: row-reverse;
   justify-content: space-between;
   gap: 2rem;
+  height: 100vh;
 
   padding: 2rem 10% 2rem 10%;
   @media (max-width: 720px) {
     flex-direction: column;
+    height: inherit;
   }
 `;
